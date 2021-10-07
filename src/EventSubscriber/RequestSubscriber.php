d5d0e3e291fdaefd48954bd152912d3eebb198be
Barys Astapau <b.astapau@andersenlab.com>
2021-10-07 06:17:45 +0000

    POL-56 Transaction integration

---
+++ src/EventSubscriber/RequestSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+class RequestSubscriber implements EventSubscriberInterface
+{
+     /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    
+
+    private $methods = ['POST', 'PUT', 'PATCH', 'DELETE'];
+
+    public function __construct(
+        EntityManagerInterface $entityManager
+    ) {
+        $this->entityManager = $entityManager;
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $conn = $this->entityManager->getConnection();
+        $method = $event->getRequest()->getMethod();
+        if (in_array($method,$this->methods))
+        {
+            $conn->beginTransaction();
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.request' => 'onKernelRequest',
+        ];
+    }
+}
