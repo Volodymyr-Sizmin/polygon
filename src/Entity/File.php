63b27b0f0554dd6690c7dc6f8b7ff37ed4d51cb3
SavelievMiron <savelevmiron@gmail.com>
2021-11-17 17:34:35 +0200

    connected File with User as ManyToOne relationship

--- src/Entity/File.php
+++ src/Entity/File.php
@@ -44,6 +44,11 @@ class File
      */
     private $updated_at;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="profilePhoto")
+     */
+    private $user;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -105,6 +110,18 @@ class File
     public function setUpdatedAt(\DateTime $updated_at): self
     {
         $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
