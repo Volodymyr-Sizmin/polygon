7033aa8012bbec2905ee524bb88109b0c252c2b3
SavelievMiron <savelevmiron@gmail.com>
2021-11-23 14:30:47 +0200

    updated

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -2,19 +2,57 @@
 
 namespace App\Tests;
 
+use App\Entity\Playlist;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class UserTest extends KernelTestCase
 {
+    private $user;
+
+    public function setUp(): void
+    {
+        $this->user = new User();
+    }
+
     public function testSpaceRemoval(): void
     {
-        $user = new User();
-        $user->setFirstName(' Name   ');
-        $user->setLastName('        Фамилия');
-        $user->setUserName(' User   Name  123  ');
-        $this->assertSame('Name', $user->getFirstName());
-        $this->assertSame('Фамилия', $user->getLastName());
-        $this->assertSame('User Name 123', $user->getUserName());
+        $this->user->setFirstName(' Name   ');
+        $this->user->setLastName('        Фамилия');
+        $this->user->setUserName(' User   Name  123  ');
+        $this->assertSame('Name', $this->user->getFirstName());
+        $this->assertSame('Фамилия', $this->user->getLastName());
+        $this->assertSame('User Name 123', $this->user->getUserName());
+    }
+
+    public function testActionsWithUserRoles()
+    {
+        $roles = $this->user->getRoles();
+        $this->assertIsArray($roles);
+
+        $this->user->setRoles(['ROLE_TESTUSER', 'ROLE_TESTADMIN']);
+        $roles = $this->user->getRoles();
+        $this->assertContains('ROLE_TESTUSER', $roles);
+        $this->assertContains('ROLE_TESTADMIN', $roles);
+
+        $this->user->setRole('ROLE_TESTMODERATOR');
+        $this->assertContains('ROLE_TESTMODERATOR', $this->user->getRoles());
+
+        $this->user->removeRole('ROLE_TESTUSER');
+        $this->assertNotContains('ROLE_TESTUSER', $roles);
+    }
+
+    public function testActionsWithPlaylist()
+    {
+        $collection = $this->user->getPlaylists();
+        $this->assertInstanceOf('Doctrine\Common\Collections\ArrayCollection', $collection);
+
+        $playlist = new Playlist();
+
+        $this->user->addPlaylist($playlist);
+        $this->assertTrue($collection->contains($playlist));
+
+        $this->user->removePlaylist($playlist);
+        $this->assertFalse($collection->contains($playlist));
     }
 }
