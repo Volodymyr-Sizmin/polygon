249119e8f5b9a0b93fdc3bc835b00c8e75842d0a
m.saveliev <savelevmiron@gmail.com>
2021-11-26 17:39:41 +0200

    created

---
+++ tests/Database/Repositories/ResetRequestRepositoryTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Database\Repositories;
+
+use App\Tests\Database\DatabaseTestCase;
+use PHPUnit\Framework\TestCase;
+
+class ResetRequestRepositoryTest extends DatabaseTestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
