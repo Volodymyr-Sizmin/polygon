3124c234e500b9313f1677cb0ca2795bf22d4988
Lenar Zairov <l.zairov@andersenlab.com>
2021-12-02 13:03:02 +0300

    Fixed remark

--- migrations/Version20211125085928.php
+++ migrations/Version20211125085928.php
@@ -19,7 +19,6 @@ final class Version20211125085928 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE playlists_tracks (playlist_id INT NOT NULL, track_id INT NOT NULL, PRIMARY KEY(playlist_id, track_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE files DROP FOREIGN KEY files_file_types_id_fk');
         $this->addSql('DROP INDEX files_file_types_id_fk ON files');
@@ -27,7 +26,6 @@ final class Version20211125085928 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE playlists_tracks');
         $this->addSql('ALTER TABLE files ADD CONSTRAINT files_file_types_id_fk FOREIGN KEY (type_id) REFERENCES file_types (id) ON UPDATE CASCADE ON DELETE CASCADE');
         $this->addSql('CREATE INDEX files_file_types_id_fk ON files (type_id)');
