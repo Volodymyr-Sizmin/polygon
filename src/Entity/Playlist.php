089c0e73d87305baf7dd307ad65592e6356ca0f7
m.saveliev <savelevmiron@gmail.com>
2021-12-03 17:38:47 +0200

    added constraints on fields; added get/set for cover; bind playlist with file;

--- src/Entity/Playlist.php
+++ src/Entity/Playlist.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\PlaylistRepository;
 use Doctrine\ORM\Mapping as ORM;
 use DateTimeImmutable;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=PlaylistRepository::class)
@@ -20,8 +21,48 @@ class Playlist
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Sequentially({
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 60,
+     *      minMessage = "Must be {{ limit }} characters or more",
+     *      maxMessage = "Must be {{ limit }} characters or less"
+     * ),
+     * @Assert\Regex(
+     *      pattern = "/^[a-zа-я0-9!@#$%^&*()_\-=+;:'\x22?,<>[\]{}\\\|\/№!~ ]+\.{0,1}[a-zа-я0-9!@#$%^&*()_\-=+;:'\x22?,<>[\]{}\\\|\/№!~ ]+$/iu",
+     *      message = "Can contain letters, numbers, !@#$%^&*()_-=+;:'""?,<>[]{}\|/№!~' symbols, and one dot not first or last"
+     * )
+     * })
      */
     private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity=File::class, mappedBy="playlist")
+     * @ORM\Column(type="bigint")
+     */
+    private $cover;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @Assert\Sequentially({
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 255,
+     *      minMessage = "Must be {{ limit }} characters or more",
+     *      maxMessage = "Must be {{ limit }} characters or less"
+     * ),
+     * @Assert\Regex(
+     *      pattern = "/^[a-zа-я0-9!@#$%^&*()_\-=+;:'\x22?,<>[\]{}\\\|\/№!~ ]+\.{0,1}[a-zа-я0-9!@#$%^&*()_\-=+;:'\x22?,<>[\]{}\\\|\/№!~ ]+$/iu",
+     *      message = "Can contain letters, numbers, !@#$%^&*()_-=+;:'""?,<>[]{}\|/№!~' symbols, and one dot not first or last"
+     * )
+     * })
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="playlists")
+     */
+    private $author;
 
     /**
      * @ORM\Column(type="datetime_immutable", name="created_at")
@@ -32,16 +73,6 @@ class Playlist
      * @ORM\Column(type="datetime_immutable", name="updated_at")
      */
     private $updatedAt;
-
-    /**
-     * @ORM\Column(type="text", nullable=true)
-     */
-    private $description;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="playlists")
-     */
-    private $author;
 
     public function getId(): ?int
     {
@@ -56,6 +87,42 @@ class Playlist
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCover(): ?File
+    {
+        return $this->cover;
+    }
+
+    public function setCover(?File $cover): self
+    {
+        $this->cover = $cover;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $user): self
+    {
+        $this->author = $user;
 
         return $this;
     }
@@ -84,27 +151,4 @@ class Playlist
         return $this;
     }
 
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(?string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getAuthor(): ?User
-    {
-        return $this->author;
-    }
-
-    public function setAuthor(?User $user): self
-    {
-        $this->author = $user;
-
-        return $this;
-    }
 }
