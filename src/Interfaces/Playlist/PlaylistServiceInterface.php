9840f71dc91fd4e62fb162978cbc3cd44477729c
m.saveliev <savelevmiron@gmail.com>
2021-12-03 17:40:02 +0200

    added createPlaylist method declaration

--- src/Interfaces/Playlist/PlaylistServiceInterface.php
+++ src/Interfaces/Playlist/PlaylistServiceInterface.php
@@ -2,8 +2,12 @@
 
 namespace App\Interfaces\Playlist;
 
+use App\Entity\Playlist;
+
 interface PlaylistServiceInterface{
 
     public function indexService():array;
+
+    public function createPlaylist(array $data):Playlist;
     
 }
