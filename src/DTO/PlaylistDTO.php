ea47b943d4821252ab118128e87f990fb990f3b4
m.saveliev <savelevmiron@gmail.com>
2021-12-03 17:40:30 +0200

    created DTO for Playlist

---
+++ src/DTO/PlaylistDTO.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\DTO;
+
+class PlaylistDTO
+{
+    public $cover;
+
+    public $title;
+
+    public $description;
+
+    public $author;
+
+    public $created_at;
+
+    public $update_at;
+}
---
+++ src/DTO/Transformer/PlaylistTransformerDTO.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\DTO\Transformer;
+
+use App\DTO\PlaylistDTO;
+use Symfony\Component\HttpFoundation\Request;
+
+class PlaylistTransformerDTO
+{
+    public function transform(Request $request): PlaylistDTO
+    {
+        $dto = new PlaylistDTO;
+
+        $dto->title = $request->get('title');
+        $dto->description = $request->get('description');
+
+        return $dto;
+    }
+}
