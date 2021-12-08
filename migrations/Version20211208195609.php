ae6fada8cbf43da4f9b72ad01c0e5892c8c85d71
Anton Mihai <a.mihai@andersenlab.com>
2021-12-08 20:05:20 +0000

    changed migration

--- migrations/Version20211208195609.php
+++ migrations/Version20211208195609.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211130165918 extends AbstractMigration
+final class Version20211208195609 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,10 +22,9 @@ final class Version20211130165918 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE city (id INT AUTO_INCREMENT NOT NULL, country_id INT NOT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_2D5B0234F92F3E70 (country_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE country (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE track (id INT AUTO_INCREMENT NOT NULL, author VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, album VARCHAR(255) DEFAULT NULL, type VARCHAR(25) NOT NULL, genre VARCHAR(255) NOT NULL, cover VARCHAR(255) DEFAULT NULL, track_path VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', playlist_id INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE city ADD CONSTRAINT FK_2D5B0234F92F3E70 FOREIGN KEY (country_id) REFERENCES country (id)');
-        $this->addSql('ALTER TABLE files DROP FOREIGN KEY files_file_types_id_fk');
-        $this->addSql('DROP INDEX files_file_types_id_fk ON files');
-        $this->addSql('ALTER TABLE user ADD city_id INT DEFAULT NULL, CHANGE profilePhoto profilePhoto BIGINT DEFAULT 0 NULL');
+        $this->addSql('ALTER TABLE user ADD city_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6498BAC62AF FOREIGN KEY (city_id) REFERENCES city (id)');
         $this->addSql('CREATE INDEX IDX_8D93D6498BAC62AF ON user (city_id)');
     }
@@ -37,9 +36,8 @@ final class Version20211130165918 extends AbstractMigration
         $this->addSql('ALTER TABLE city DROP FOREIGN KEY FK_2D5B0234F92F3E70');
         $this->addSql('DROP TABLE city');
         $this->addSql('DROP TABLE country');
-        $this->addSql('ALTER TABLE files ADD CONSTRAINT files_file_types_id_fk FOREIGN KEY (type_id) REFERENCES file_types (id) ON UPDATE CASCADE ON DELETE CASCADE');
-        $this->addSql('CREATE INDEX files_file_types_id_fk ON files (type_id)');
+        $this->addSql('DROP TABLE track');
         $this->addSql('DROP INDEX IDX_8D93D6498BAC62AF ON user');
-        $this->addSql('ALTER TABLE user DROP city_id, CHANGE profilePhoto profilePhoto BIGINT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user DROP city_id');
     }
 }
