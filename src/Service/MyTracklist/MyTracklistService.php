a796517cd5f1cdaf504c6f3f7720168c45714b6c
m.saveliev <savelevmiron@gmail.com>
2021-12-13 19:18:53 +0200

    fixed formatting

--- src/Service/MyTracklist/MyTracklistService.php
+++ src/Service/MyTracklist/MyTracklistService.php
@@ -1,5 +1,6 @@
 <?php
-namespace App\Service\MyTraclist;
+
+namespace App\Service\MyTracklist;
 
 use App\Interfaces\MyTracklist\MyTracklistInterface;
 use Doctrine\ORM\EntityManagerInterface;
@@ -8,10 +9,7 @@ use App\Service\FileUploader;
 use phpDocumentor\Reflection\Types\Null_;
 use Symfony\Component\Filesystem\Filesystem;
 
-/**
- * @TODO rename correcly TRACK not TRAC
- */
-class MyTraclistService implements MyTracklistInterface
+class MyTracklistService implements MyTracklistInterface
 {
     private $entityManager;
     private $trackRepository;
@@ -19,39 +17,40 @@ class MyTraclistService implements MyTracklistInterface
 
     public function __construct(EntityManagerInterface $entityManagerInterface, FileUploader $fileUploader)
     {
-        $this->fileUploader =$fileUploader;
+        $this->fileUploader = $fileUploader;
         $this->entityManager = $entityManagerInterface;
         $this->trackRepository = $this->entityManager->getRepository(Track::class);
     }
+
     public function indexService()
     {
         return $this->trackRepository->findAll();
     }
 
-    public function createService():array
+    public function createService(): array
     {
         return array([
             'trackType' => ['Book', 'Podcast', 'Music'],
             'genreType' => [
                 'Rock',
-                'Pop', 
-                'Classical', 
-                'Jazz', 
-                'Blues', 
-                'Hip-Hop', 
-                'Hardcore', 
-                'Metal', 
-                'Trance', 
-                'House', 
-                'Punk', 
-                'Grunge', 
+                'Pop',
+                'Classical',
+                'Jazz',
+                'Blues',
+                'Hip-Hop',
+                'Hardcore',
+                'Metal',
+                'Trance',
+                'House',
+                'Punk',
+                'Grunge',
                 'Folk',
                 "Drum'n'bass",
-                'Russian Chanson', 
-                'Retro', 
-                'Funk', 
-                'Ethnic', 
-                'Reggae', 
+                'Russian Chanson',
+                'Retro',
+                'Funk',
+                'Ethnic',
+                'Reggae',
                 'Lounge'
             ]
         ]);
@@ -72,7 +71,7 @@ class MyTraclistService implements MyTracklistInterface
         if ($tracklistDTO->track_path !== null) {
             $track->setTrackPath($this->fileUploader->upload($tracklistDTO->track_path)->getUrl());
         }
- 
+
         if ($tracklistDTO->title !== null) {
             $track->setTitle($tracklistDTO->title);
         }
@@ -80,11 +79,11 @@ class MyTraclistService implements MyTracklistInterface
         if ($tracklistDTO->author !== null) {
             $track->setAuthor($tracklistDTO->author);
         }
-        
+
         if ($tracklistDTO->type !== null) {
             $track->setType($tracklistDTO->type);
         }
-        
+
         if ($tracklistDTO->genre !== null) {
             $track->setGenre($tracklistDTO->genre);
         }
@@ -96,89 +95,76 @@ class MyTraclistService implements MyTracklistInterface
     }
 
     public function showService($id)
-    {   
-
-        if ($this->trackRepository->find($id) == NULL ) 
-        {
-                        return array([
-                'success' => false,
-                'body'    => 'Can not find track'
-            ]); 
-        }
-        return $this->trackRepository->find($id);
-
-    }
-
-    public function editService($id)
     {
-
-        if ($this->trackRepository->find($id) == NULL ) 
-        {
-                        return array([
-                'success' => false,
-                'body'    => 'Can not find track'
-            ]); 
-        }
-        return $this->trackRepository->find($id);
-
-    }
-
-    public function updateService($tracklistDTO, $track)
-    {
-        $fileSystem = new Filesystem;
-
-        if($tracklistDTO->cover !== NULL)
-        {
-            $fileSystem->remove('../public/uploads/'.$track->getCover());
-            $track->setCover($this->fileUploader->upload($tracklistDTO->cover)->getUrl());
-        }
-
-        if($tracklistDTO->album !== NULL)
-        {
-            $track->setAlbum($tracklistDTO->album);
-        }
-
-        if($tracklistDTO->title !== NULL)
-        {
-            $track->setTitle($tracklistDTO->title);
-        }
-
-        if ($tracklistDTO->author !== NULL) 
-        {
-            $track->setAuthor($tracklistDTO->author);
-        }
-
-        if ($tracklistDTO->type !== NULL) 
-        {
-            $track->setType($tracklistDTO->type);
-        }
-
-        if ($tracklistDTO->genre !== NULL) 
-        {
-            $track->setGenre($tracklistDTO->genre);
-        }
-        
-        $this->entityManager->flush();
-      
-        return $track;
-    }
-
-    public function deleteService($id)
-    {
-        if ($this->trackRepository->find($id) == NULL ) 
-        {
+        if ($this->trackRepository->find($id) == null) {
             return array([
                 'success' => false,
                 'body'    => 'Can not find track'
             ]);
         }
-        
+        return $this->trackRepository->find($id);
+    }
+
+    public function editService($id)
+    {
+        if ($this->trackRepository->find($id) == null) {
+            return array([
+                'success' => false,
+                'body'    => 'Track not found'
+            ]);
+        }
+        return $this->trackRepository->find($id);
+    }
+
+    public function updateService($tracklistDTO, $track)
+    {
+        $fileSystem = new Filesystem();
+
+        if ($tracklistDTO->cover !== null) {
+            $fileSystem->remove('../public/uploads/' . $track->getCover());
+            $track->setCover($this->fileUploader->upload($tracklistDTO->cover)->getUrl());
+        }
+
+        if ($tracklistDTO->album !== null) {
+            $track->setAlbum($tracklistDTO->album);
+        }
+
+        if ($tracklistDTO->title !== null) {
+            $track->setTitle($tracklistDTO->title);
+        }
+
+        if ($tracklistDTO->author !== null) {
+            $track->setAuthor($tracklistDTO->author);
+        }
+
+        if ($tracklistDTO->type !== null) {
+            $track->setType($tracklistDTO->type);
+        }
+
+        if ($tracklistDTO->genre !== null) {
+            $track->setGenre($tracklistDTO->genre);
+        }
+
+        $this->entityManager->flush();
+
+        return $track;
+    }
+
+    public function deleteService($id)
+    {
+        if ($this->trackRepository->find($id) == null) {
+            return array([
+                'success' => false,
+                'body'    => 'Track not found'
+            ]);
+        }
+
         $this->entityManager->remove($this->trackRepository->find($id));
         $this->entityManager->flush();
 
         return array([
-            'success' => true, 
-            'body'    => 'Track successfully deleted'
+            'success' => true,
+            'body'    => 'Track was successfully deleted'
         ]);
     }
 }
