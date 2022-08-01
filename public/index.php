a00a424006eddff37db12b29534cf948cadecb32
a.brutov <a.brutov@andersenlab.com>
2022-08-01 08:44:10 +0000

    Fixed some issues

--- public/index.php
+++ public/index.php
@@ -4,14 +4,6 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-if ($_SERVER['APP_DEBUG']) {
-    header('Access-Control-Allow-Origin:'.rtrim($_SERVER['HTTP_REFERER'], '/'));
-} else {
-    header('Access-Control-Allow-Origin:yourdomain');
-}
-header('Access-Control-Allow-Headers:*');
-header('Access-Control-Allow-Credentials:true');
-header('Access-Control-Allow-Headers:X-Requested-With, Content-Type, withCredentials');
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     die();
 }
