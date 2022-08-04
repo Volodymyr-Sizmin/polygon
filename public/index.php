e8440ac5546c4bd8fb248d8ec97b4d029a358e73
a.brutov <a.brutov@andersenlab.com>
2022-08-04 08:52:48 +0000

    Made some changes

--- public/index.php
+++ public/index.php
@@ -4,10 +4,6 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    die();
-}
-
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
