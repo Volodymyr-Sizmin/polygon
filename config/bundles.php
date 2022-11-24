d4cf0d1ccd73d253c17d610fb5bca6e6f72682cf
Pavlo Herasymchuk <p.herasymchuk@andersenlab.com>
2022-11-24 11:31:42 +0000

    Revert "Merge branch 'Add_symfony_profiler_/_cors' into 'develop'"
    This reverts merge request !193

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,4 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
