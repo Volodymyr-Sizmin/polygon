d289a53f7bb588bba056c3d53c2e0278d1623fbb
Pavel Herasymchuk <p.herasymchuk@andersenlab.com>
2022-12-08 17:17:58 +0200

    Add return type declaration

--- src/Service/CookieService.php
+++ src/Service/CookieService.php
@@ -6,6 +6,10 @@ use Symfony\Component\HttpFoundation\Cookie;
 
 class CookieService
 {
+    /**
+     * @param $value
+     * @return Cookie
+     */
     public function setCookie($value)
     {
         return new Cookie('UserCookie', $value, time() + 600, '/', null, false, false, false, 'lax');
