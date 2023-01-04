ec6dd7cfcb7aed8e488dae0d46fef12f46595b60
Christina Asipenka <c.asipenka@andersenlab.com>
2023-01-04 16:18:38 +0100

    added endpoints for TopUpPhone page

--- src/Controller/CardsController.php
+++ src/Controller/CardsController.php
@@ -2,14 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\Cards;
-use App\Entity\User;
 use App\Service\CardsInfoService;
-use App\Service\TokenService;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,99 +18,28 @@ class CardsController extends AbstractController
         $this->CardsInfoService = $CardsInfoService;
     }
 
-
-
     /**
-     * @Route("/cards_service/{email}/cards", name="cardslist", methods={"GET"})
-     * @param $email
-     * @param Response $response
-     * @param ManagerRegistry $doctrine
+     * @Route("/payments_and_transfers/{email}/cards", name="cardslist", methods={"GET"})
+     * @param string $email
      * @return JsonResponse|void
      */
-
-  ///  public function cardsList(Request $request, Response $response, ManagerRegistry $doctrine)
-    public function cardsList(string $email, Response $response, ManagerRegistry $doctrine, Request $request)
+    public function cardsList(string $email) : JsonResponse
     {
-
         $response = $this->CardsInfoService->getCardsInfo($email);
 
-       // return new Response(, 200);
-return $response;
-        //списала, показалось нужным
+        if (count($response)>0) {
+            $jsonStr = [
+                'success' => true,
+                'cards' => $response
+            ];
 
-     /*   $session = $request->getSession();
-        if ($session->get('attempts') >= 3) {
-            return new JsonResponse(
-                [
-                    'success' => true,
-                    'attempts' => 'limit',
-                ],
-                403
-            );
-        }*/
-
-      //  $data = json_decode($request->getContent(), true);
-
-       /* if (empty($data['email'])) {
-            return new JsonResponse(
-                [
-                    'success' => false,
-                    'body' => [
-                        'message' => 'Empty input',
-                    ],
-                ],
-                404
-            );
-        }*/
-
-      //  $entityManager = $doctrine->getManager();
-      //  $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
-
-       /* if (isset($user)) {   //try
-            $cards = $entityManager->getRepository(Cards::class)->findOneBy(['client_id' => $user->getId()]);
-
-            if(isset($cards)){
-                return new JsonResponse(
-                    [
-                        'success' => true,
-                        'body' => [
-                            'balance'=> $cards ->getBalance(),
-                            'cardType'=> $cards ->getCardType(),
-                            'clientId'=> $cards ->getClientId(),
-                            'currency'=> $cards ->getCardCurrency(),
-                            'expirationDate'=> $cards ->getExpirationDate(),
-                            'id'=> $cards ->getId(),
-                            'limit'=> $cards ->getCardLimit(),
-                            'name'=> $cards ->getCardName(),
-                            'number'=> $cards ->getCardNumber(),
-                            'status'=> $cards ->getCardStatus(),
-
-                        ],
-                    ],
-                    200
-                );
-            } else {
-                return new JsonResponse(
-                    [
-                        'success' => true,
-                        'body' => [ ],
-                    ],
-                    200
-                );
-            }
-
+            return new JsonResponse($jsonStr, Response::HTTP_OK);
         } else {
-            return new JsonResponse(
-                [
-                    'success' => false,
-                    'body' => [
-                        'message' => 'Hello. There is not any user to ger card list.',
-                    ],
-                ],
-                200
-            );
-        }*/
+            $jsonStr = [
+                'success' => true
+            ];
 
-
+            return new JsonResponse($jsonStr, Response::HTTP_NO_CONTENT);
+        }
     }
 }
