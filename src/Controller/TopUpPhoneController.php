add3c64584d15e2841a1f14e5ea4b01b9c69c72f
Christina Asipenka <c.asipenka@andersenlab.com>
2023-01-10 12:21:26 +0100

    endpoint for payment button

--- src/Controller/TopUpPhoneController.php
+++ src/Controller/TopUpPhoneController.php
@@ -1,26 +1,36 @@
 <?php
-
 namespace App\Controller;
 
-use App\Entity\CellPhoneOperators;
-use App\Service\CardsInfoService;
-use Doctrine\Persistence\ManagerRegistry;
+use App\Service\PhonePaymentService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TopUpPhoneController extends AbstractController
 {
-    protected $CardsInfoService;
-    private $entityManager;
+    public Request $params;
+    private PhonePaymentService $phonePaymentService;
 
-    public function __construct(CardsInfoService $CardsInfoService, ManagerRegistry $doctrine)
+    public function __construct(PhonePaymentService $phonePaymentService)
     {
-        $this->CardsInfoService = $CardsInfoService;
-        $this -> entityManager = $doctrine->getManager();
+        $this->phonePaymentService = $phonePaymentService;
     }
 
+    /**
+     * @Route("/service/payments/{email}/topupphone", name="topupphone", methods={"POST"})
+     * @param $params{'phone_number', 'id_operator', 'amount','cardNumber'}
+     * @return JsonResponse
+     */
 
-    public function index(){
-        $cellPhoneOperatorsList = $this ->entityManager->getRepository(CellPhoneOperators::class)->findAll();
+    public function phonePayment(string $email, Request $params) : JsonResponse
+    {
+
+        $data = json_decode($params->getContent(), true);
+        $result = $this->phonePaymentService->PhonePayment($email, $data);
+
+        return new JsonResponse($result,Response::HTTP_OK);
     }
 
 }
