3161368fd7d2ba0cbdf7dc250fe63e137c824c74
Ihor Volikov <ih.volikov@andersenlab.com>
2023-01-12 13:06:57 +0200

    PN-1346 Added table "Payments" and all related tables and entities

---
+++ migrations/Version20230110164618.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230110164618 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $table = $schema->createTable('payments');
+        $table->addColumn('id', 'integer', [
+            'autoincrement' => 1,
+            'notnull' => 1,
+            'unsigned' => 1,
+        ]);
+        $table->addColumn('user_id', 'biginteger', [
+            'notnull' => 1
+        ]);
+        $table->addColumn('subject', 'string', [
+            'length' => 255,
+            'notnull' => 1
+        ]);
+        $table->addColumn('account_debit_id', 'biginteger', [
+            'notnull' => 1
+        ]);
+        $table->addColumn('account_credit_id', 'biginteger', [
+            'notnull' => 1
+        ]);
+        $table->addColumn('amount', 'integer', [
+            'notnull' => 1
+        ]);
+        $table->addColumn('currency_id', 'integer', [
+            'notnull' => 1
+        ]);
+        $table->addColumn('created_at', 'timestamp', [
+            'notnull' => 1
+        ]);
+        $table->addColumn('status_id', 'integer', [
+            'notnull' => 1
+        ]);
+        $table->addColumn('type_id', 'integer', [
+            'notnull' => 1
+        ]);
+        $table->setPrimaryKey(['id']);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $schema->dropTable('payments');
+    }
+}
---
+++ src/Entity/Currency.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CurrencyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="currencies")
+ * @ORM\Entity(repositoryClass=CurrencyRepository::class)
+ */
+class Currency
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $full_name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFullName(): ?string
+    {
+        return $this->full_name;
+    }
+
+    public function setFullName(string $full_name): self
+    {
+        $this->full_name = $full_name;
+
+        return $this;
+    }
+}
