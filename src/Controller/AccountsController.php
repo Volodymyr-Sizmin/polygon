4991ba0b0ba0ab213d468e0c15b25dd3028a7f91
Christina Asipenka <c.asipenka@andersenlab.com>
2023-01-20 15:51:13 +0100

    1.3

--- src/Controller/AccountsController.php
+++ src/Controller/AccountsController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class AccountsController extends AbstractController
 {
@@ -20,23 +21,37 @@ class AccountsController extends AbstractController
     }
 
     /**
-     * @Route("/{email}/accounts", name= "app_accounts", methods={"GET"})
+     * @Route("/accounts/{email}", name= "app_accounts", methods={"GET"})
      */
     public function getAccounts(string $email): JsonResponse
     {
         $accounts = $this->em->getRepository(Account::class)->findBy(['user_id'=>$email]);
+        $account_arr=[];
+        /** @var  $one_account  Account */
+        foreach ($accounts as $one_account){
 
-        return new JsonResponse($accounts, Response::HTTP_OK);
+            $foreResponse=[
+                'id' => $one_account->getId(),
+                'user_id' => $one_account->getUserId(),
+                'number'=> $one_account->getNumber(),
+                'currency_id'=>$one_account->getCurrencyId()
+            ];
+
+            array_push($account_arr,$foreResponse);
+        }
+        return new JsonResponse($account_arr, Response::HTTP_OK);
     }
 
     /**
      *@Route("/accounts/{email}/{number}", name="one_account", methods={"GET"})
+     * @return JsonResponse
      */
 
-    public function getAccount(string $email, string $number): JsonResponse
+    public function getAccount(string $email, string $number, SerializerInterface $serializer): JsonResponse
     {
+        /** @var  $account  Account */
         $account = $this->em->getRepository(Account::class)->findOneBy(['number'=>$number]);
-
-        return new JsonResponse($account, Response::HTTP_OK);
+        $content = $serializer->serialize($account,'json');
+        return new JsonResponse(json_decode($content), Response::HTTP_OK);
     }
 }
