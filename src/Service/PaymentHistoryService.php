fad3926e90d29b627310f22567caded16acb3a41
Ihor Volikov <ih.volikov@andersenlab.com>
2023-01-23 15:58:31 +0200

    PN-1530 filtering history of payments (minor code improvements)

--- src/Service/PaymentHistoryService.php
+++ src/Service/PaymentHistoryService.php
@@ -10,28 +10,8 @@ class PaymentHistoryService
     {
         $matchEmail = $decodedToken->data->email;
         $data = json_decode($request->getContent(), true);
-        $params = ['user_id' => $matchEmail];
+        $data['user_id'] = $matchEmail;
 
-        if(isset($data['amount'])) {
-            $params['amount'] = $data['amount'];
-        }
-
-        if(isset($data['currency'])) {
-            $params['currency'] = $data['currency'];
-        }
-
-        if(isset($data['created_at'])) {
-            $params['created_at'] = $data['created_at'];
-        }
-
-        if(isset($data['status_id'])) {
-            $params['status_id'] = $data['status_id'];
-        }
-
-        if(isset($data['type_id'])) {
-            $params['type_id'] = $data['type_id'];
-        }
-
-        return $doctrine->getRepository(Payment::class)->findBy($params);
+        return $doctrine->getRepository(Payment::class)->findBy($data);
     }
 }
--- tests/Controller/PaymentHistoryServiceTest.php
+++ tests/Controller/PaymentHistoryServiceTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\Controller;
 
+use App\Service\PaymentHistoryService;
+use App\Service\TokenService;
 use Faker\Factory;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -20,6 +22,30 @@ class PaymentHistoryServiceTest extends WebTestCase
      */
     public function testFilterHistoryOfPayments(): void
     {
+        //$container = self::bootKernel();
+        /*
+        // (1) boot the Symfony kernel
+        self::bootKernel();
+
+        // (2) use static::getContainer() to access the service container
+        $container = static::getContainer();
+
+        $newsRepository = $this->createMock(TokenService::class);
+        $newsRepository->expects(self::once())
+            ->method('decodeToken')
+            ->willReturn([
+                new \stdClass(),
+                new News('some other news'),
+            ])
+        ;
+
+        // the following line won't work unless the alias is made public
+        $container->set(NewsRepositoryInterface::class, $newsRepository);
+
+        // will be injected the mocked repository
+        $newsletterGenerator = $container->get(NewsletterGenerator::class);
+        */
+
         $this->client->jsonRequest('GET', '/payments_and_transfers/filter_history');
         $response = $this->client->getResponse();
         $this->assertSame(200, $response->getStatusCode());
