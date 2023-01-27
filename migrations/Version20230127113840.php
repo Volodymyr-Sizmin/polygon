33f96fb25e38a68d7abb8d33134d2417fe8450d3
Yevgeniy Bistritsky <yevgeniybistritsky@gmail.com>
2023-01-27 14:06:29 +0200

    Entity(Account): change cardNubmer to string(16)

--- migrations/Version20230127113840.php
+++ migrations/Version20230127113840.php
@@ -20,7 +20,7 @@ final class Version20230127113840 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE accounts ADD card_number VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE accounts ADD card_number VARCHAR(16) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
