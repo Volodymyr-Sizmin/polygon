10aaa2a0214dcd3ed90c6737242e49ca5b286046
Pavel Herasymchuk <p.herasymchuk@andersenlab.com>
2023-01-27 10:50:49 +0200

    PN-1652 Bugs fixed

--- migrations/Version20230125113804.php
+++ migrations/Version20230125113804.php
@@ -20,7 +20,8 @@ final class Version20230125113804 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE autopayments ADD created_at DATETIME NOT NULL');;
+        $this->addSql('ALTER TABLE autopayments ADD created_at DATETIME');
+        $this->addSql('ALTER TABLE autopayments ADD updated_at DATETIME');
         $this->addSql('ALTER TABLE payment_types ADD on_the_main_page INT NOT NULL, CHANGE id id INT AUTO_INCREMENT NOT NULL');
     }
 
@@ -28,6 +29,7 @@ final class Version20230125113804 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE autopayments DROP created_at');
+        $this->addSql('ALTER TABLE autopayments DROP updated_at');
         $this->addSql('ALTER TABLE payment_types DROP on_the_main_page, CHANGE id id INT UNSIGNED AUTO_INCREMENT NOT NULL');
      }
 }
