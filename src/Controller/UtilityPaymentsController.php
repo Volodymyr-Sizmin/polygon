0aac93a053aeaf96fb29de1fa10b7008cf7fca85
Yevgeniy Bistritsky <yevgeniybistritsky@gmail.com>
2023-01-31 17:09:44 +0200

    Refactor: remove abundant migration and imports

--- src/Controller/UtilityPaymentsController.php
+++ src/Controller/UtilityPaymentsController.php
@@ -14,8 +14,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validation;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UtilityPaymentsController extends AbstractController
 {
