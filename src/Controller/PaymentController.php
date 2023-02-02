4f63fc08de13a86797f8eb6efa1ff7e616fd6342
Ihor Volikov <ih.volikov@andersenlab.com>
2023-02-02 17:37:18 +0200

    PN-1346 history of payments, PN-1536 sorting payments history

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -26,14 +26,14 @@ class PaymentController extends AbstractController
     /**
      * @Route("/payments_and_transfers/history", name="history", methods={"GET"})
      */
-    public function getHistoryOfPayments(ManagerRegistry $managerRegistry, SerializerInterface $serializer): JsonResponse
+    public function getHistoryOfPayments(ManagerRegistry $doctrine, SerializerInterface $serializer, Request $request): JsonResponse
     {
-        //$data = $managerRegistry->getManager()->getRepository(Payment::class)->findAll();
-        //!!! mockData for frontend. Development of this code will continue.
-        $mockData = [['id' => 1, 'subject' => 'Birthday_gift', 'account_debit_number' => '1111222233334444', 'account_credit_number' => '2222333344445555', 'amount' => 10500, 'currency' => 'GBP', 'user_id' => 1, 'status_id' => 'completed', 'created_at' => date('d.m.Y H:i:s', strtotime('10.01.2023 14:16:29'))],
-            ['id' => 2, 'subject' => 'Top_up_phone', 'account_debit_number' => '3333444455556666', 'account_credit_number' => '1111222233334444', 'amount' => 250, 'currency' => 'GBP', 'user_id' => 1, 'status_id' => 'in_processing', 'created_at' => date('d.m.Y H:i:s', strtotime('11.01.2023 10:38:05'))],
-            ['id' => 3, 'subject' => 'Some_payment', 'account_debit_number' => '1111222233334444', 'account_credit_number' => '4444555566667777', 'amount' => 1890, 'currency' => 'GBP', 'user_id' => 1, 'status_id' => 'success_failed', 'created_at' => date('d.m.Y H:i:s', strtotime('12.01.2023 16:56:45'))]];
-        $result = $serializer->serialize($mockData, 'json');
+        $token = $this->tokenService->getToken($request);
+        $decodedToken = $this->tokenService->decodeToken(substr($token, 7));
+
+        $data = $this->paymentHistoryService->getHistoryOfPayments($decodedToken, $doctrine);
+
+        $result = $serializer->serialize($data, 'json');
 
         return new JsonResponse($result, Response::HTTP_OK, [], true);
     }
@@ -47,6 +47,20 @@ class PaymentController extends AbstractController
         $decodedToken = $this->tokenService->decodeToken(substr($token, 7));
 
         $payments = $this->paymentHistoryService->getFilteredHistoryOfPayments($decodedToken, $request, $doctrine);
+        $result = $serializer->serialize($payments, 'json');
+
+        return new JsonResponse($result, Response::HTTP_OK, [], true);
+    }
+
+    /**
+     * @Route("/payments_and_transfers/sort_history", name="sort_history", methods={"GET"})
+     */
+    public function sortHistoryOfPayments(ManagerRegistry $doctrine, SerializerInterface $serializer, Request $request): JsonResponse
+    {
+        $token = $this->tokenService->getToken($request);
+        $decodedToken = $this->tokenService->decodeToken(substr($token, 7));
+
+        $payments = $this->paymentHistoryService->getSortedHistoryOfPayments($decodedToken, $request, $doctrine);
         $result = $serializer->serialize($payments, 'json');
 
         return new JsonResponse($result, Response::HTTP_OK, [], true);
--- src/Service/PaymentHistoryService.php
+++ src/Service/PaymentHistoryService.php
@@ -14,4 +14,83 @@ class PaymentHistoryService
 
         return $doctrine->getRepository(Payment::class)->findBy($data);
     }
+
+    public function getHistoryOfPayments($decodedToken, $doctrine):array
+    {
+        $matchEmail = $decodedToken->data->email;
+
+        $query = " 
+        SELECT 
+            payments.*,
+            currencies.name as currency,   
+            payment_statuses.name as status,
+            payment_types.name as payment_type  
+        FROM 
+            payments
+        LEFT JOIN 
+            currencies ON payments.currency_id = currencies.id
+        LEFT JOIN 
+            payment_statuses ON payments.status_id = payment_statuses.id
+        LEFT JOIN 
+            payment_types ON payments.type_id = payment_types.id
+        WHERE 
+            payments.user_id = :email
+        ORDER BY 
+            payments.created_at DESC
+        LIMIT 1";
+
+        $connection = $doctrine->getConnection("default");
+        return $connection
+            ->prepare($query)
+            ->executeQuery(['email' => $matchEmail])
+            ->fetchAllAssociative();
+    }
+
+    public function getSortedHistoryOfPayments($decodedToken, $request, $doctrine):array
+    {
+        $matchEmail = $decodedToken->data->email;
+        $data = json_decode($request->getContent(), true);
+
+        if(!$data) {
+            $data = [];
+        }
+
+        $arraySize = count($data);
+        $orderString = "";
+        $iterator = 0;
+
+        foreach ($data as $key => $value) {
+            $iterator++;
+            $orderString = ($iterator == $arraySize) ? $orderString . "payments." . $key . " " . $value . " " : $orderString . "payments." . $key . " " . $value . ", ";
+        }
+
+        if($orderString === "") {
+            $orderString = "payments.created_at desc ";
+        }
+
+        $query ="  
+        SELECT 
+            payments.*,
+            currencies.name as currency,   
+            payment_statuses.name as status,
+            payment_types.name as payment_type  
+        FROM 
+            payments
+        LEFT JOIN 
+            currencies ON payments.currency_id = currencies.id
+        LEFT JOIN 
+            payment_statuses ON payments.status_id = payment_statuses.id
+        LEFT JOIN 
+            payment_types ON payments.type_id = payment_types.id
+        WHERE 
+            payments.user_id = :email
+        ORDER BY " . $orderString . "
+        LIMIT 1";
+
+        $connection = $doctrine->getConnection("default");
+        return $connection
+            ->prepare($query)
+            ->executeQuery(['email' => $matchEmail])
+            ->fetchAllAssociative();
+    }
 }
